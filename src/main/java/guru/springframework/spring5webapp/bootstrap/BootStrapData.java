c6b78c906a559542d62f28ed01379f5864a9e2c0
KarnaukhovSE <50140038+KarnaukhovSE@users.noreply.github.com>
2021-03-02 23:48:59 +0300

    complete 18 step

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -32,25 +32,6 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "123123");
-        eric.getBooks().add(ddd);
-        ddd.getAuthors().add(eric);
-
-        authorRepository.save(eric);
-        bookRepository.save(ddd);
-
-        Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE development without EJB", "99876876675");
-        rod.getBooks().add(noEJB);
-        noEJB.getAuthors().add(rod);
-
-        authorRepository.save(rod);
-        bookRepository.save(noEJB);
-
-        System.out.println("Started in Bootstrap");
-        System.out.println("Number of Books: " + bookRepository.count());
-
         Publisher publisher = new Publisher(
                 "Publisher Name",
                 "Publisher Address",
@@ -60,8 +41,35 @@ public class BootStrapData implements CommandLineRunner {
         );
         publisherRepository.save(publisher);
 
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+        publisher.getBooks().add(ddd);
+        ddd.setPublisher(publisher);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+        publisherRepository.save(publisher);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE development without EJB", "99876876675");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+        publisher.getBooks().add(noEJB);
+        noEJB.setPublisher(publisher);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+
         System.out.println("Number of Publshers: " + publisherRepository.count());
         Iterable<Publisher> iterable = publisherRepository.findAll();
         iterable.forEach(publisher1 -> System.out.println(publisher1.toString()));
+
+        System.out.println("Number of publishers books: " + publisher.getBooks().size());
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -14,6 +14,9 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(
             name = "author_book",
@@ -62,6 +65,14 @@ public class Book {
         this.authors = authors;
     }
 
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -83,6 +94,7 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
+                ", publisher=" + publisher +
                 ", authors=" + authors +
                 '}';
     }
